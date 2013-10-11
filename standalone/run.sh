92db2e4dbb46eeb94bb59c87913afca25ef288e0
Patricia Gorla <gorla.patricia@gmail.com>
2013-10-11 01:13:36 +0000

    Update run script with init options.

--- standalone/run.sh
+++ standalone/run.sh
@@ -8,26 +8,39 @@ JVM_OPTS="$JVM_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
 JVM_OPTS="$JVM_OPTS -Dcom.sun.management.jmxremote.ssl=false"
 JVM_OPTS="$JVM_OPTS -Djava.rmi.server.hostname=$SERVER_HOSTNAME"
 
+TARGET="target/usergrid-standalone-0.0.28-SNAPSHOT.jar"
+TARGET_TEST="target/usergrid-standalone-0.0.28-SNAPSHOT-tests.jar"
+
+CMD="java -jar $JVM_OPTS"
+
 case $1 in
 
     run)
-        echo "Running usergrid standalone alone"
-        nohup java -jar $JVM_OPTS target/usergrid-standalone-0.0.28-SNAPSHOT.jar &
+        echo "Run standalone"
+        nohup $CMD $TARGET &
         ;;
     run-db)
-        echo "Running usergrid standalone with embedded cassandra"
-        nohup java -jar $JVM_OPTS target/usergrid-standalone-0.0.28-SNAPSHOT.jar -db &
+        echo "Running standalone with embedded cassandra"
+        nohup $CMD $TARGET -db &
+        ;;
+    run-init)
+        echo "Run standalone and initialize embedded cassandra"
+        nohup $CMD $TARGET -db -init &
         ;;
     test)
-        echo "Running usergrid standalone alone with test"
-        nohup java -jar $JVM_OPTS target/usergrid-standalone-0.0.28-SNAPSHOT-tests.jar &
+        echo "Test standalone"
+        nohup $CMD $TARGET_TEST &
         ;;
     test-db)
-        echo "Running usergrid standalone alone with test and embedded cassandra"
-        nohup java -jar $JVM_OPTS target/usergrid-standalone-0.0.28-SNAPSHOT-tests.jar -db &
+        echo "Test standalone with embedded cassandra"
+        nohup $CMD $TARGET_TEST -db &
+        ;;
+    test-init)
+        echo "Test standalone and initialize embedded cassandra"
+        nohup $CMD $TARGET_TEST -db &
         ;;
     *)
-        echo "Usage: $0 {run|test|run-db|test-db}" >&2
+        echo "Usage: $0 {run|run-db|run-init|test|test-db|test-init}" >&2
         exit 1
         ;;
 esac
